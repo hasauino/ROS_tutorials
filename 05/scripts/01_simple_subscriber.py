dfd4f8d9140f4ce253a5cc8cd8c6ef6f6c75dcf0
hasauino <oh91@windowslive.com>
2019-09-15 15:44:14 +0200

    تحديث

---
+++ 05/scripts/01_simple_subscriber.py
@@ -0,0 +1,13 @@
+import rospy
+from std_msgs.msg import String
+
+def cb(msg):
+    print msg.data
+
+
+rospy.init_node('mostame3')
+
+rospy.Subscriber('myTopic', String, callback=cb)
+
+while not rospy.is_shutdown():
+    pass
---
+++ 05/scripts/02_simple_subscriber.py
@@ -0,0 +1,12 @@
+import rospy
+from std_msgs.msg import String
+
+def cb(msg):
+    print msg.data
+
+
+rospy.init_node('mostame3')
+
+rospy.Subscriber('myTopic', String, callback=cb)
+
+rospy.spin()
