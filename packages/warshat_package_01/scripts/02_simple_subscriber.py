990a25d972ddbc74e63b998b4bbbe6f1bfc4cdd3
hasauino <oh91@windowslive.com>
2019-09-17 23:24:03 +0200

    إضافة مجلد للباكيجات الي كتبناها

---
+++ packages/warshat_package_01/scripts/02_simple_subscriber.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def cb(msg):
+    print msg.data
+
+
+rospy.init_node('mostame3')
+
+rospy.Subscriber('myTopic', String, callback=cb)
+
+rospy.spin()
---
+++ packages/warshat_package_01/scripts/commander.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+import rospy
+from util import Turtle
+
+if __name__ == "__main__":
+    t1 = Turtle()
+
+    r = 1.0 #m
+    w = 1.0 #rad/sec
+
+    v = w*r #m/s
+
+    while not rospy.is_shutdown():
+        t1.set_velocity(v, w)
+        print '-------------'
+        print t1.color
+
